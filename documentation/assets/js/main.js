a4d5dac97e1bd9733fe39ec99c9c91651913c8df
Gustavo <gbalieirof@gmail.com>
2021-04-02 09:28:52 -0300

    1.6

--- documentation/assets/js/main.js
+++ documentation/assets/js/main.js
@@ -1,64 +1,61 @@
-$(document).ready(function() {
-    function createCookie(name, value, days) {
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            var expires = "; expires=" + date.toGMTString();
-        } else var expires = "";
-        document.cookie = name + "=" + value + expires + "; path=/";
-    };
-    var getUrlParameter = function getUrlParameter(sParam) {
-        var sPageURL = window.location.search.substring(1),
-            sURLVariables = sPageURL.split('&'),
-            sParameterName,
-            i;
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+};
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
-        for (i = 0; i < sURLVariables.length; i++) {
-            sParameterName = sURLVariables[i].split('=');
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
 
-            if (sParameterName[0] === sParam) {
-                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
 
-            }
         }
-    };
+    }
+};
 
-    function getCookie(name) {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return parts.pop().split(';').shift();
-    };
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+};
 
-    [
-        "campaign_id",
-        "action_code",
-        "name",
-        "currency"
-    ]
-    .forEach(function(inf) {
+[
+    "campaign_id",
+    "action_code",
+    "name",
+    "currency"
+]
+.forEach(function(inf) {
 
-        if (getUrlParameter(inf) != null) {
-            createCookie("Integrada-" + inf, getUrlParameter(inf), 30);
-        }
-    });
+    if (getUrlParameter(inf) != null) {
+        createCookie("Integrada-" + inf, getUrlParameter(inf), 30);
+    }
+});
 
-    // var cookie = "";
-    // var campaing_ID = getUrlParameter('campaign_id');
-    // createCookie("Campaing", cookie + campaing_ID, 30);
-    // var action_Code = getUrlParameter('action_code');
-    // createCookie("ActionCode", cookie + action_Code, 30);
+// var cookie = "";
+// var campaing_ID = getUrlParameter('campaign_id');
+// createCookie("Campaing", cookie + campaing_ID, 30);
+// var action_Code = getUrlParameter('action_code');
+// createCookie("ActionCode", cookie + action_Code, 30);
 
-    // campaignID = getCookie('Campaing');
-    // actionCode = getCookie('ActionCode');
+// campaignID = getCookie('Campaing');
+// actionCode = getCookie('ActionCode');
 
 
-    // document.getElementById("id_camp").innerHTML = campaignID;
-    // document.getElementById("action_code").innerHTML = actionCode;
+// document.getElementById("id_camp").innerHTML = campaignID;
+// document.getElementById("action_code").innerHTML = actionCode;
 
-    // document.getElementById("id_camp_px").innerHTML = campaing_ID;
-    // document.getElementById("action_code_px").innerHTML = actionCode;
-})
-
+// document.getElementById("id_camp_px").innerHTML = campaing_ID;
+// document.getElementById("action_code_px").innerHTML = actionCode;
 function copyPixel() {
     var copyText = document.getElementById("test");
     copyText.select();
