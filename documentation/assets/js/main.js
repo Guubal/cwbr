53cd816f2b5987e3751d0303c5323ae2fa708ad7
Gustavo <gbalieirof@gmail.com>
2021-04-01 21:49:24 -0300

    àdded cookie

--- documentation/assets/js/main.js
+++ documentation/assets/js/main.js
@@ -1,19 +1,47 @@
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+$(document).ready(function() {
+    function createCookie(name, value, days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            var expires = "; expires=" + date.toGMTString();
+        } else var expires = "";
+        document.cookie = name + "=" + value + expires + "; path=/";
+    };
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+
+            }
         }
-    }
-};
-var campaingID = getUrlParameter('campaing_id');
-var actionCode = getUrlParameter('action_code');
+    };
+    var cookie = "";
+    var campaing_ID = getUrlParameter('campaign_id');
 
-console.log(campaingID);
-console.log(actionCode);
+    createCookie("Campaing", cookie + campaing_ID, 1000);
+
+    var actionCode = getUrlParameter('action_code');
+
+    console.log(actionCode);
+    console.log(campaing_ID);
+
+    document.getElementById("id_camp").innerHTML = campaing_ID;
+    document.getElementById("action_code").innerHTML = actionCode;
+
+    document.getElementById("id_camp_px").innerHTML = campaing_ID;
+    document.getElementById("action_code_px").innerHTML = actionCode;
+})
+
+function copyPixel() {
+    var copyText = document.getElementById("test");
+    copyText.select();
+    document.execCommand("copy");
+}
+//       $('.card').addClass('test');
